899ca64a6da60fa57a0d4ab287562d9866c2026e
Thor Wei Jie <thorweijie@yahoo.com>
2021-11-26 17:22:58 +0800

    Change format for database URI

--- engine/base.py
+++ engine/base.py
@@ -2,7 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("")
+# Replace host, password, port and user based on system configuration
+database = "coqui_training"
+host = "localhost"
+password = "password"
+port = "3306"
+user = "root"
+
+engine = create_engine(
+    f"mariadb+mariadbconnector://{user}:{password}@{host}:{port}/{database}"
+)
 session = sessionmaker(bind=engine)
 
 Base = declarative_base()
